8435b6ad34934e95831d55d43e873cf6f796748b
Pardip Lal <pardiplaljio@gmail.com>
2026-03-30 20:18:25 +0530

    unused ui hide

--- components/kia-overlays/kia-context-menu/registry/layer.js
+++ components/kia-overlays/kia-context-menu/registry/layer.js
@@ -1,69 +1,69 @@
 const Index = [
 
-{
-		action: 'bringToFront',
-		title: 'Bring To Front',
-		icon: {
-			name: 'bring-to-front',
-		},
-		shortcut: '['
-	},
+	// {
+	// 	action: 'bringToFront',
+	// 	title: 'Bring To Front',
+	// 	icon: {
+	// 		name: 'bring-to-front',
+	// 	},
+	// 	shortcut: '['
+	// },
 
-	{
-		action: 'sendToBack',
-		title: 'Send To Back',
-		icon: {
-			name: 'send-to-back',
-		},
-		shortcut: ']'
-	},
+	// {
+	// 	action: 'sendToBack',
+	// 	title: 'Send To Back',
+	// 	icon: {
+	// 		name: 'send-to-back',
+	// 	},
+	// 	shortcut: ']'
+	// },
 
-	{
-		action: 'lockUnlock',
-		title: 'Lock/Unlock',
-		icon: {
-			name: 'lock',
-		},
-		shortcut: 'Ctrl + L'
-	},
+	// {
+	// 	action: 'lockUnlock',
+	// 	title: 'Lock/Unlock',
+	// 	icon: {
+	// 		name: 'lock',
+	// 	},
+	// 	shortcut: 'Ctrl + L'
+	// },
 
-	{
-		action: 'hideShow',
-		title: 'Hide/Show',
-		icon: {
-			name: 'eye',
-		},
-		shortcut: 'Ctrl + H'
-	},
+	// {
+	// 	action: 'hideShow',
+	// 	title: 'Hide/Show',
+	// 	icon: {
+	// 		name: 'eye',
+	// 	},
+	// 	shortcut: 'Ctrl + H'
+	// },
 
-	{
-		action: 'duplicate',
-		title: 'Duplicate',
-		icon: {
-			name: 'clone-solid-full',
-			fill: 'currentColor',
-		},
-		shortcut: 'Ctrl + D'
-	},
+	// {
+	// 	action: 'duplicate',
+	// 	title: 'Duplicate',
+	// 	icon: {
+	// 		name: 'clone-solid-full',
+	// 		fill: 'currentColor',
+	// 	},
+	// 	shortcut: 'Ctrl + D'
+	// },
 
-	{
-		action: 'copy',
-		title: 'Copy',
-		icon: {
-			name: 'copy',
-		},
-		shortcut: 'Ctrl + C'
-	},
+	// {
+	// 	action: 'copy',
+	// 	title: 'Copy',
+	// 	icon: {
+	// 		name: 'copy',
+	// 	},
+	// 	shortcut: 'Ctrl + C'
+	// },
 
-	{
-		action: 'paste',
-		title: 'Paste',
-		icon: {
-			name: 'paste-solid-full',
-			fill: 'currentColor',
-		},
-		shortcut: 'Ctrl + V'
-	},	
+	// {
+	// 	action: 'paste',
+	// 	title: 'Paste',
+	// 	icon: {
+	// 		name: 'paste-solid-full',
+	// 		fill: 'currentColor',
+	// 	},
+	// 	shortcut: 'Ctrl + V'
+	// },
 
 	{
 		action: 'delete',
