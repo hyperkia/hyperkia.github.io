b3891ac112c6425e90329c6d4d8cfef36261aa38
Pardip Lal <pardiplaljio@gmail.com>
2026-03-30 20:04:19 +0530

    zip default css  update

--- transformer/fflateZip/utils/props.js
+++ transformer/fflateZip/utils/props.js
@@ -11,8 +11,8 @@ const Index = {
 
           <style>
             /* Default */
-            .page{background-color: #fff;position: relative;overflow: hidden;isolation: isolate;user-select: none;transform-origin: 0 0;}
-            .canvas-layer{background: #d9d9d9;stroke:none;stroke-width: 1px;fill: #d9d9d9;user-select: none;transition: none;caret-color: transparent;pointer-events: auto;display: block;overflow-wrap: anywhere;white-space: pre-wrap;border: 0px solid #000000;position: absolute;}
+            .page{background-color: #fff;position: relative;overflow: hidden;isolation: isolate;user-select: auto;transform-origin: 0 0;}
+            .canvas-layer{background: #d9d9d9;stroke:none;stroke-width: 1px;fill: #d9d9d9;user-select: auto;transition: none;caret-color: transparent;pointer-events: auto;display: block;overflow-wrap: anywhere;white-space: pre-wrap;border: 0px solid #000000;position: absolute;}
             .page-svg, svg.canvas-layer {fill: transparent;background-color: transparent;}
 
             /* Img layer */
@@ -20,7 +20,7 @@ const Index = {
             .canvas-layer.imghtml[src=""]{background: url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='24' height='24' viewBox='0 0 24 24' fill='none' stroke='%23fff' stroke-width='2' stroke-linecap='round' stroke-linejoin='round' class='feather feather-image'%3E%3Crect x='3' y='3' width='18' height='18' rx='2' ry='2'/%3E%3Ccircle cx='8.5' cy='8.5' r='1.5'/%3E%3Cpolyline points='21 15 16 10 5 21'/%3E%3C/svg%3E") center center no-repeat #687787;background-size: 65% auto;}
 
             /* Text Layer */
-            .texthtml{background: transparent;cursor: context-menu;user-select: none;caret-color: transparent;fill: transparent;line-height: normal;}
+            .texthtml{background: transparent;cursor: auto;user-select: auto;caret-color: transparent;fill: transparent;line-height: normal;}
             .texthtml:focus-visible{outline: none;}
             .texthtml[contenteditable="true"]{cursor: auto;user-select: auto;caret-color: #383838;}
           </style>  
@@ -30,7 +30,7 @@ const Index = {
 
             .menu-pages{position: fixed;padding: 20px;top:20px;left:20px;z-index: 999;}
 
-            .menu-pages-btn{background: #0d99ff; color: #fff; border: none;width: 35px;height: 35px;cursor: pointer;position: relative;isolation: isolate;cursor: pointer;}
+            .menu-pages-btn{background: #0d99ff; color: #fff; border: none;width: 35px;height: 35px;cursor: pointer;position: relative;isolation: isolate;}
             .menu-pages-btn svg{width: 20px;}
             .menu-pages-btn input{position: absolute;top: 0;left: 0;width: 100%;height: 100%;opacity: 0;z-index: 1;}
 
